4bcb8a1f6c57132e0b8b425cbb287cd6e82ed500
StevenMacias <stevenmcs22@gmail.com>
2024-05-31 10:55:36 -0400

    add fruit model

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
+
 const express = require("express");
 
+const mongoose = require("mongoose");
+
+
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  });
+
+
+const Fruit = require("./models/fruit.js");
+
 
 app.get("/", async (req, res) => {
     res.render("index.ejs");
