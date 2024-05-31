b8e368f127da184a1543f18749ac8d4fbf712da8
StevenMacias <stevenmcs22@gmail.com>
2024-05-31 11:28:17 -0400

    add new fruit view

--- server.js
+++ server.js
@@ -17,11 +17,31 @@ mongoose.connection.on("connected", () => {
 
 const Fruit = require("./models/fruit.js");
 
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get("/", async (req, res) => {
     res.render("index.ejs");
-  });
+});
+
+app.get("/fruits", (req, res) => {
+    res.send("Welcome to the index page!");
+});
   
+app.get("/fruits/new", (req, res) => {
+    res.render("fruits/new.ejs");
+});
+
+app.post("/fruits", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+    res.redirect("/fruits/new");
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
